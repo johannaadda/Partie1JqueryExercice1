cd727e3cf199bad9bf8294b63130af96fba9b3b9
johannaadda <johannaadda@icloud.com>
2020-05-07 03:06:27 -0700

    final Ex1

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// base du Jquery a inclure toujours en debut (comme le html), $(document).ready(function() est la version longue , utliser version courte comme ci dessous.
+$(function(){
+// Cibler id text pour modifier l element par hide
+$('#text').show();
+});
